54dadf7f824d5fd013561df8e9336f4a8c33ca48
Roman Amb <roman83mars@gmail.com>
2024-03-22 01:47:12 +0300

    task of lesson 16

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
 'use strict'
 
 class First {
-    constructor() {
-
-    }
     hello() {
         console.log('Привет я метод родителя!');
     }
 }
 
 class Second extends First {
-    constructor() {
-
+    hello() {
+        super.hello()
+        console.log('А я наследуемый метод!');
     }
 }
+
+const second = new Second()
+
+second.hello()
